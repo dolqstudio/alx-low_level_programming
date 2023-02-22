c0e781c2e9a122a226819aedc6fd931f230b8240
Olawunmi <dolqstudio@gmail.com>
2023-02-22 05:27:59 -0800

    main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - main function
+ * function that prints the alphabet, in lowercase, followed by a new line.
+ * Return: 0 always
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
